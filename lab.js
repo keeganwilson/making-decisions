bb02b69032f7d46cf6a487327fd4afe24d3ba57d
Keegan Wilson <keegan.wilson@gmail.com>
2022-03-22 16:12:29 -0600

    Completed first 4 problems

--- lab.js
+++ lab.js
@@ -10,7 +10,7 @@
 let lovesCode = true;
 if (lovesCode === true) {
   console.log(`I love to code!`)
-} else console.log(`Coding has its chanllenges`);
+} else console.log(`Coding has its challenges`);
 
 // For problems 2-3 use the following lines of code:
 var amysAge = 29;
@@ -25,7 +25,12 @@ var brittanisBirthYear = 1986;
 */
 
 //CODE HERE
-
+if (amysAge > brittanisAge) {
+  console.log('Amy is older')
+  else if (amysAge < brittanisAge) {
+    console.log('Brittani is older')
+  } else console.log('Amy and Brittani are the same age.')
+}
 ////////// PROBLEM 3 //////////
 
 /*
@@ -33,7 +38,9 @@ var brittanisBirthYear = 1986;
 */
 
 //CODE HERE
-
+if (amysBirthYear === brittanisBirthYear) {
+  console.log('Amy and Brittani were born in the same year.')
+} else console.log('Amy and Brittani were not born in the same year')
 ////////// PROBLEM 4 //////////
 
 let temperature = 55;
